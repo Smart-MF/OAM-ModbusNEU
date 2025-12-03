e9ef8afdb94cb77600a7c6590373793cb7ec4bf2
Waldemar Porscha <wp@porscha.eu>
2025-12-03 10:46:48 +0100

    Update project setup - .gitignore: remove knxprod from delivery - OpenKXN-Build-Sttings: Typo in Return statement and add correct xml-Filename (because it differs from Module-Name) - ModbusGateway.xml: Updated versions

--- scripts/OpenKNX-Build-Settings.ps1
+++ scripts/OpenKNX-Build-Settings.ps1
@@ -1,7 +1,6 @@
 # set product names, allows mapping of (devel) name in Project to a more consistent name in release
 $settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "ModbusModule" "ModbusModule"
 
-$settings.sourceName = "ModbusModule"  
-$settings.targetName = "ModbusModule"
+$settings.releaseName = "ModbusGateway"
 
-Return $settingsS
+Return $settings
